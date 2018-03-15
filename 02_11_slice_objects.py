fded71605980fe707fe6a3c37c6fffb280c154a3
pvosper <pvosper@mac.com>
2018-03-15 12:49:05 -0700

    Lunchtime homework

--- 02_11_slice_objects.py
+++ 02_11_slice_objects.py
@@ -2,9 +2,7 @@
 # Example 2-11 Line items from a flat-file invoice
 
 invoice = """
-         1          2         3         4         5         6
-123456789012345678990123456789012345678901234567890123456789012345
-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890
 |-..-||-..............................-||-........-||-||-....-|
 1909  Pimoroni PiBrella                       $17.50  3  $52.50
 1489  6mm Tactile Switch x20                   $4.95  2   $9.90
@@ -13,10 +11,20 @@ ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
 """
 
 SKU = slice(0, 6)
+type(SKU)
+# Out[12]: slice
+
 DESCRIPTION = slice(6, 40)
 UNIT_PRICE = slice(40, 52)
 QUANTITY = slice(52, 55)
 ITEM_TOTAL = slice(55, None)
-line_items = invoice.split('\n')[2:]
+line_items = invoice.split('\n')    # [2:]
 for item in line_items:
     print(item[UNIT_PRICE], item[DESCRIPTION])
+
+# opqrstuvwxyz GHIJKLMNOPQRSTUVWXYZabcdefghijklmn
+# |-........-| |-..............................-|
+#       $17.50 Pimoroni PiBrella
+#        $4.95 6mm Tactile Switch x20
+#       $28.00 Panavise Jr. - PV-201             
+#       $34.95 PiTFT Mini Kit 320x240
---
+++ 02_12_lists_of_lists.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Example 2-12 A list with three lists of length 3 can represent a tic-tac-toe board
+
+# Create a list of three lists of three items each. Inspect the structure
+board = [['_'] * 3 for i in range(3)]
+
+board
+# Out[2]: [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+
+# Place a mark in row 1, column 2 and check the result
+
+# Example 2-13 A list with three references to teh same list is useless
+weird_board = [['_'] * 3] * 3
+
+weird_board
+# Out[6]: [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+
+
+weird_board[1][2] = '0'
+
+weird_board
+# Out[8]: [['_', '_', '0'], ['_', '_', '0'], ['_', '_', '0']]
+
+# Each iteration builds a new row and appends it to board
